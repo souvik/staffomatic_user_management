d516b7046f8646f34e38c50a7df41aaf83b63bd9
Souvik Kumar Khan <souvik.khan@ymail.com>
2021-09-16 11:59:27 +0530

    Added code coverage tool

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,24 +7,23 @@ class UsersController < ApplicationController
   end
 
   def archive
-    user = User.find(params[:id])
-    user.update_status_by_action(params[:action], current_user)
-    render jsonapi: user
+    modify_status
   end
 
   def unarchive
-    user = User.find(params[:id])
-    user.update_status_by_action(params[:action], current_user)
-    render jsonapi: user
+    modify_status
   end
 
   def destroy
+    modify_status
+  end
+
+  private
+  def modify_status
     user = User.find(params[:id])
     user.update_status_by_action(params[:action], current_user)
     render jsonapi: user
   end
-
-  private
 
   def handle_self_update_error(exception)
     unprocessable_entity_error(exception.message)
